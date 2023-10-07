20b20432debfde1d0728507974a6eacb08f8ffbc
«Vladimir_U» <ulzutuev.vl@mail.ru>
2023-10-07 10:23:13 +0300

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# sf_data_science_VldUlz
-SF Homework Vladimir Ulzutuev
---
+++ module4_pandas/module4.py
@@ -0,0 +1,7 @@
+simplelist = [19, 242, 14, 152, 142, 1000]
+sum = 0
+k = 0
+for i in simplelist:
+    sum += i
+    k += 1
+print(sum/k)
